f5b7b0708e14a4b53d5964d2263e216a8ee95a3a
atsushi-h <atsushi.1213eng@gmail.com>
2019-12-25 19:44:02 +0900

    ボタンにアイコンを追加

--- src/components/button/DeleteMylistBtn.js
+++ src/components/button/DeleteMylistBtn.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const DeleteMylistBtn = props => {
   return (
     <Button
       variant="contained"
       color="secondary"
+      startIcon={<DeleteIcon />}
       onClick={() => props.deleteMylist(props.movie)}
     >
       DELETE
